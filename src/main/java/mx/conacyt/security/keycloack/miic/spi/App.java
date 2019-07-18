94d3531a628f19d5160d0c705db96b7f0f63d3e8
danimaniarqsoft <daniel.cortes.pichardo@gmail.com>
2019-07-18 13:25:13 -0500

    intial commit

---
+++ src/main/java/mx/conacyt/security/keycloack/miic/spi/App.java
@@ -0,0 +1,12 @@
+package mx.conacyt.security.keycloack.miic.spi;
+
+/**
+ * Simple main application
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
